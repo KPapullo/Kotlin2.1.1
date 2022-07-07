026feb9fbfd753883d1612892a63a00be0e42900
Ekaterina Papullo <vazhno@bk.ru>
2022-07-07 16:05:38 +0300

    All tests passed

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -4,7 +4,7 @@ import org.junit.Assert.*
 class MainKtTest {
 
     @Test
-    fun commission() {
+    fun maestroFreeLimit() {
         // arrange
 
         val amount1 = 100_000
@@ -19,7 +19,7 @@ class MainKtTest {
 
     }
     @Test
-    fun commissionNoFreeLimit() {
+    fun maestroNoFreeLimit() {
         // arrange
 
         val amount1 = 100_000
@@ -33,4 +33,111 @@ class MainKtTest {
         assertEquals(2600, result)
 
     }
+    @Test
+    fun mastercardFreeLimit() {
+        // arrange
+
+        val amount1 = 100_000
+        val amountSum1 = 0
+        val typeCard1 = "Mastercard"
+
+        // act
+        val result = commission(amount = amount1, amountSum = amountSum1, typeCard = typeCard1)
+
+        // assert
+        assertEquals(0, result)
+
+    }
+    @Test
+    fun mastercardNoFreeLimit() {
+        // arrange
+
+        val amount1 = 100_000
+        val amountSum1 = 10_000_000
+        val typeCard1 = "Mastercard"
+
+        // act
+        val result = commission(amount = amount1, amountSum = amountSum1, typeCard = typeCard1)
+
+        // assert
+        assertEquals(2600, result)
+
+    }
+    @Test
+    fun visaUnderMinCommission() {
+        // arrange
+
+        val amount1 = 100_000
+        val amountSum1 = 0
+        val typeCard1 = "VISA"
+
+        // act
+        val result = commission(amount = amount1, amountSum = amountSum1, typeCard = typeCard1)
+
+        // assert
+        assertEquals(3500, result)
+
+    }
+    @Test
+    fun visaUpperMinCommission() {
+        // arrange
+
+        val amount1 = 1_000_000
+        val amountSum1 = 10_000_000
+        val typeCard1 = "VISA"
+
+        // act
+        val result = commission(amount = amount1, amountSum = amountSum1, typeCard = typeCard1)
+
+        // assert
+        assertEquals(7500, result)
+
+    }
+    @Test
+    fun mirUnderMinCommission() {
+        // arrange
+
+        val amount1 = 100_000
+        val amountSum1 = 0
+        val typeCard1 = "MIR"
+
+        // act
+        val result = commission(amount = amount1, amountSum = amountSum1, typeCard = typeCard1)
+
+        // assert
+        assertEquals(3500, result)
+
+    }
+    @Test
+    fun mirUpperMinCommission() {
+        // arrange
+
+        val amount1 = 1_000_000
+        val amountSum1 = 10_000_000
+        val typeCard1 = "MIR"
+
+        // act
+        val result = commission(amount = amount1, amountSum = amountSum1, typeCard = typeCard1)
+
+        // assert
+        assertEquals(7500, result)
+
+    }
+    @Test
+    fun vkPayCommission() {
+        // arrange
+
+        val amount1 = 100_000
+        val amountSum1 = 20_000_000
+        val typeCard1 = "VK pay"
+
+        // act
+        val result = commission(amount = amount1, amountSum = amountSum1, typeCard = typeCard1)
+
+        // assert
+        assertEquals(0, result)
+
+    }
+
+
 }
